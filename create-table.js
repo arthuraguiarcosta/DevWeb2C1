1e51fad6203cc184920528c853d22ce3d3d4a410
Arthur Aguiar <50185969+arthuraguiarcosta@users.noreply.github.com>
2020-04-29 19:09:40 -0300

    Add files via upload

---
+++ create-table.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql2');
+const faker = require('faker');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3336,
+    user: 'root',
+    password: '123456',
+    database: 'dbC1'
+});
+
+connection.connect(function(err){
+    if(err) return console.log(err);
+    console.log('Conectado ao Banco de dados!');
+    createTableUsers(connection);
+    populateUsers(connection);
+});
+
+function createTableUsers(conn){
+    const sql = `CREATE TABLE IF NOT EXISTS Users
+                   (id INT NOT NULL AUTO_INCREMENT, 
+                   nome VARCHAR(200) NOT NULL,
+                   email VARCHAR(100) NOT NULL,
+                   telefone VARCHAR(100) NOT NULL,
+                   altura VARCHAR(50) NOT NULL,
+                   peso VARCHAR(200) NOT NULL,
+                   saude VARCHAR(50) NOT NULL,
+                   PRIMARY KEY (id)
+                   );`
+    conn.query(sql, function(error, results, fields){
+        if(error) return console.log(error);
+        console.log('Tabela criada com sucesso!')
+    })
+}
+
+function populateUsers(conn){
+    const sql = `INSERT INTO Users(nome, email, telefone, altura, peso, saude) VALUES ?`;
+    
+    let values = [];
+
+    for(let i = 0; i < 10; i++){
+        let altura = (Math.random() * 0.45 + 1.50).toString+"m";  
+        let peso = (Math.random() * 80 + 45).toString+"kg";  
+        let arraySaude = ["Saúde OK","COVID 19","Dengue","Virose","Obesidade"]
+        let saude = arrSaude[Math.floor(Math.random() * arr.length)];  
+        values.push([faker.name.findName(), faker.internet.email(), faker.phone.phoneNumber(), altura, peso, saude]);
+    }
+
+    conn.query(sql, [values], function(error, results, fields){
+        if(error) return console.log(error);
+        console.log('Registros inseridos com sucesso!');
+        conn.end();
+    });
+}
---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+const mysql = require('mysql2');
+
+// npm install express --save
+// npm install body-parser --save
+
+//Configurações do ambiente
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Criando um roteador
+const router = express.Router();
+router.get('/', (req, res) => res.json({message: 'API rodando!'}));
+app.use('/', router);
+
+function execQuery(query, res){
+  const connection = mysql.createConnection({
+      host: '127.0.0.1',
+      port: 3336,
+      user: 'root',
+      password: '123456',
+      database: 'dbC1'
+  });
+
+  connection.query(query, function(error, results, fields){
+    
+    if(error) res.json(error);
+    else res.json(results);
+    
+    connection.end();
+    console.log('Query executada com sucesso!')
+  });
+}
+
+//Rotas Users
+router.get('/users', (req, res) => {
+  execQuery('SELECT * FROM Users;', res);
+});
+
+router.get('/users/:id?', (req, res) => {
+  let filter = '';
+  if(req.params.id) filter = ' WHERE id = ' + parseInt(req.params.id);
+  execQuery('SELECT * FROM Users ' + filter, res);
+});
+
+router.delete('/users/:id', (req, res) => {
+  execQuery('DELETE FROM Users WHERE id=' + parseInt(req.params.id), res);
+});
+
+router.post('/users', (req, res) => {
+  const nome = req.body.nome.substring(0,200);
+  const email = req.body.email.substring(0,100);
+  const telefone = req.body.telefone.substring(0,50);
+  const altura = req.body.altura.substring(0,50);
+  const peso = req.body.peso.substring(0,200);
+  const saude = req.body.saude.substring(0,50);
+  execQuery(`INSERT INTO Users (nome, email, telefone, altura, peso, saude) VALUES ('${nome}', '${email}', '${telefone}', '${altura}', '${peso}', '${saude}');`, res);
+});
+
+router.patch('/users/:id', (req,res) => {
+  const nome = req.body.nome.substring(0,200);
+  const email = req.body.email.substring(0,100);
+  const telefone = req.body.telefone.substring(0,50);
+  const altura = req.body.altura.substring(0,50);
+  const peso = req.body.peso.substring(0,200);
+  const saude = req.body.saude.substring(0,50);
+  execQuery(`UPDATE Users SET nome='${nome}', email='${email}', '${telefone}', '${altura}', '${peso}', '${saude}' WHERE id = '${req.params.id}';`, res);
+});
+
+//Iniciando o servidor
+app.listen(port);
+console.log('API rodando ....');
+
+
+
+
+
+
+
+
